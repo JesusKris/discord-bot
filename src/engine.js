cfa428281f28531eb29735e736946c10ef07b93a
nimi25820 <robert.laur.private@gmail.com>
2022-07-12 16:46:50 +0300

    Reworked the application to use commonjs only, readded ORM with env variables, replaced all sensitive information with env variables in engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,45 +1,49 @@
-import DiscordJS, { GuildMember } from 'discord.js';
-import dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
+const env = require('dotenv');
+env.config({ path: '../.env' });
+const DiscordJS = require('discord.js');
+const { sequelize } = require('./orm/models');
+const express = require('express');
 
-const client = new DiscordJS.Client({
-	intents: [
-		'GUILD_MESSAGES',
-		'GUILDS',
 
-	],
+// Opening a webserver for endpoints.
+const app = express();
+const server = app.listen(process.env.WS_PORT, function() { // eslint-disable-line
+	console.log('WS succesfully started and listening to requests.');
 });
 
-client.login(process.env.TOKEN);
+
+// Utilizing discord client and providing intents -> what it will use/can use
+const client = new DiscordJS.Client({
+	intents: JSON.parse(process.env.BOT_INTENTS),
+});
 
 client.on('ready', () => {
-	console.log('Bot online');
+	console.log(`Succesfully started the application and logged in as ${client.user.tag}`);
 });
 
 
-/* client.on('messageCreate', message => {
-	switch (message.content.toUpperCase()) {
-	case '?RESET':
-		resetBot(message.channel);
-		break;
+// Pinging mariadb connection with sequelize to check if we get back a healthy connection
+const pingDB = async (count) => {
+	console.log('Testing database connection..');
 
-        ... other commands
+	try {
+		await sequelize.authenticate();
+		console.log('Connection to the database has been established succesfully.');
 	}
-});
-
-
-function resetBot(channel) {
-    console.log("bot restarting")
-	channel.send('Resetting...')
-		.then(msg => client.destroy())
-		.then(() => client.login(process.env.TOKEN), console.log("bot restarted"));
-
-}
-
-client.on('messageCreate', message => {
-	if (message.content.toLowerCase() == 'shutdown') {
-		message.channel.send('Shutting down...').then(() => {
+	catch (error) {
+		console.error(`Try: ${count} Unable to connect to the database:`, error);
+		if (count != 3) {
+			pingDB(count + 1);
+		}
+		else {
+			console.log('Failed to establish database connection. Shutting down..');
 			client.destroy();
-		});
+			process.exit();
+
+		}
 	}
-}); */
+};
+
+
+pingDB(1);
+client.login(process.env.BOT_TOKEN);
