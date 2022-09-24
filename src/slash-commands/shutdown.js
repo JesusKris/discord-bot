216a0fb8407f2995cde55fc9dca6f873d9e91c51
nimi25820 <robert.laur.private@gmail.com>
2022-09-24 20:44:57 +0300

    Reworked shutdown to use buttons

--- src/slash-commands/shutdown.js
+++ src/slash-commands/shutdown.js
@@ -1,19 +1,12 @@
 const { handleError } = require('../modules/errorHandling');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
 const config = require('../appconfig.js');
 const { getWarningEmbed } = require('../bot-responses/embeds/warning');
 const { getStandardEmbed } = require('../bot-responses/embeds/standard');
 
 exports.run = async (client, interaction, permissions) => {
 	try {
-		await sendConfirmationMessage(client, interaction);
-
-		const answer = await askForConfirmation(client, interaction);
-
-		if (answer) {
-			await sendResult(client, interaction, answer);
-		}
-
-
+		await askForConfirmation(client, interaction);
 	}
 	catch (error) {
 		handleError(error);
@@ -31,33 +24,54 @@ exports.config = {
 	maxArgs: 0,
 };
 
-async function sendConfirmationMessage(client, interaction) {
-	try {
-		await interaction.deferReply({ ephemeral: true, content: 'Thinking...' });
-		await interaction.editReply({
-			content: 'Are you sure you want to shut down the bot? yes/no',
-		});
-	}
-	catch (error) {
-		handleError(error);
-	}
-}
-
 async function askForConfirmation(client, interaction) {
 	try {
+		const buttons = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('no')
+					.setLabel('Cancel!')
+					.setEmoji('❌')
+					.setStyle(ButtonStyle.Secondary),
+				new ButtonBuilder()
+					.setCustomId('yes')
+					.setLabel('Yes!')
+					.setEmoji('✅')
+					.setStyle(ButtonStyle.Secondary),
+			);
 
-		const filter = m => m.content === 'yes' || m.content === 'no';
-		const answer = await interaction.channel.awaitMessages({
-			filter: filter,
-			max: 1,
-			time: config.client.commands.defaultAwaitTimer,
-			errors: ['time'],
+		await interaction.deferReply({ ephemeral: true, content: 'Thinking...' });
 
-		}).catch(async () => {
-			interaction.editReply({ embeds: [await getWarningEmbed(null, 'No correct response received.. canceling.')], content: '' });
+
+		await interaction.editReply({
+			embeds: [await getStandardEmbed(null, 'Are you sure you want to shut down the bot?')],
+			components: [buttons],
 		});
 
-		return answer;
+		const collector = await interaction.channel.createMessageComponentCollector({
+			componentType: ComponentType.Button,
+			max: '1',
+			time: config.client.commands.defaultAwaitTimer,
+		});
+
+		collector.on('collect', async (interaction) => {
+			if (interaction.customId === 'yes') {
+				await interaction.deferUpdate();
+				await sendResult(client, interaction, 'yes');
+			}
+
+			if (interaction.customId === 'no') {
+				await interaction.deferUpdate();
+				await sendResult(client, interaction, 'no');
+			}
+
+		});
+
+		collector.on('end', async (collected) => {
+			if (collected.size == 0) {
+				await interaction.editReply({ embeds: [await getWarningEmbed(null, 'Canceled the operation')], content: '', components: [] }); // Run a piece of code when the collector ends
+			}
+		});
 
 	}
 	catch (error) {
@@ -68,17 +82,15 @@ async function askForConfirmation(client, interaction) {
 async function sendResult(client, interaction, answer) {
 	try {
 
-		switch (answer.first().content) {
-		case 'yes':
-			answer.first().delete();
-			await interaction.editReply({ embeds: [await getStandardEmbed(null, 'Shutting the bot down...')], content: '' }).then(() => {
-				client.destroy();
-			});
-			process.exit(0);
+		switch (answer) {
+			case 'yes':
+				await interaction.editReply({ embeds: [await getStandardEmbed(null, 'Shutting the bot down...')], content: '', components: [] }).then(() => {
+					client.destroy();
+				});
+				process.exit(0);
 
-		case 'no':
-			answer.first().delete();
-			return await interaction.editReply({ embeds: [await getWarningEmbed(null, 'Canceled the operation')], content: '' });
+			case 'no':
+				return await interaction.editReply({ embeds: [await getWarningEmbed(null, 'Canceled the operation')], content: '', components: [] });
 		}
 	}
 	catch (error) {
