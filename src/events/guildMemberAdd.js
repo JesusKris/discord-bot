d9763942acef5ed5cb4fe6165ea8c3272b495f0b
Robert <robert.laur.private@gmail.com>
2022-10-12 17:20:58 +0300

    Refactored guildMemberAdd.js#2

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -6,17 +6,21 @@ const { shuffleArray } = require("../modules/utils.js");
 
 module.exports = async (client, member) => { // eslint-disable-line
 
-
 	if (member.user.bot) return;
 
 	const settings = await getGuildSettings(member);
+
 
 	// notifications
 	if (settings && settings.notification_channel) {
 
 		try {
 			const channel = await member.guild.channels.cache.get(settings.notification_channel);
-			channel.send({ embeds: [await getStandardEmbed("User has joined the server!", `Username: ${userMention(member.user.id)}`, { url: member.user.displayAvatarURL({ dynamic: true }) })], },);
+
+			//channel has been deleted
+			if (!channel) return;
+
+			return channel.send({ embeds: [await getStandardEmbed("User has joined the server!", `Username: ${userMention(member.user.id)}`, { url: member.user.displayAvatarURL({ dynamic: true }) })], },);
 		}
 		catch (error) {
 			handleError(error)
@@ -24,12 +28,17 @@ module.exports = async (client, member) => { // eslint-disable-line
 
 	}
 
+	
 	// greetings
 	if (settings && settings.greetings_channel && !settings.is_main) {
 
 		try {
 			const channel = await member.guild.channels.cache.get(settings.greetings_channel);
-			channel.send(await getGreetingMessage(member));
+
+			//channel has been deleted
+			if (!channel) return;
+
+			return channel.send(await getGreetingMessage(member));
 		}
 		catch (error) {
 			handleError(error)
