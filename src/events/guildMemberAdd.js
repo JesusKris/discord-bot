fb023e7635700cc1ff431ba960cd4a3f89d3e1e7
Robert <robert.laur.private@gmail.com>
2022-10-16 13:28:19 +0300

    Final refactor guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -14,40 +14,47 @@ module.exports = async (client, member) => { // eslint-disable-line
 	// notifications
 	if (settings && settings.notification_channel) {
 
-		try {
-			const channel = await member.guild.channels.cache.get(settings.notification_channel);
-
-			// channel has been deleted
-			if (!channel) return;
-
-			await channel.send({ embeds: [await getStandardEmbed("User has joined the server!", `Username: ${userMention(member.user.id)}`, { url: member.user.displayAvatarURL({ dynamic: true }) })] });
-		}
-		catch (error) {
-			handleError(error);
-		}
+		await sendNotification(settings, member)
 
 	}
-
-
+	
+	
 	// greetings
 	if (settings && settings.greetings_channel && !settings.is_main) {
 
-		try {
-			const channel = await member.guild.channels.cache.get(settings.greetings_channel);
-
-			// channel has been deleted
-			if (!channel) return;
-
-			await channel.send(await getGreetingMessage(member));
-		}
-		catch (error) {
-			handleError(error);
-		}
+		await sendSprintGreeting(settings, member)
 
 	}
-
+	
 };
 
+async function sendNotification(settings, member) {
+	try {
+		const channel = await member.guild.channels.cache.get(settings.notification_channel);
+
+		// channel has been deleted
+		if (!channel) return;
+		
+		await channel.send({ embeds: [await getStandardEmbed("User has joined the server!", `Username: ${userMention(member.user.id)}`, { url: member.user.displayAvatarURL({ dynamic: true }) })] });
+	}
+	catch (error) {
+		handleError(error);
+	}
+}
+
+async function sendSprintGreeting(settings, member) {
+	try {
+		const channel = await member.guild.channels.cache.get(settings.greetings_channel);
+		
+		// channel has been deleted
+		if (!channel) return;
+		
+		await channel.send(await getGreetingMessage(member));
+	}
+	catch (error) {
+		handleError(error);
+	}
+}
 
 async function getGreetingMessage(member) {
 	const greetings = [
